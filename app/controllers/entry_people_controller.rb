124596b51f0a79a155c77d9a2be203aaf06fe55e
Esmé Cowles <escowles@ticklefish.org>
2024-04-29 06:51:18 -0400

    improving entry forms, adding ability to add participants

--- app/controllers/entry_people_controller.rb
+++ app/controllers/entry_people_controller.rb
@@ -1,2 +1,18 @@
 class EntryPeopleController < ApplicationController
+  # POST /entriy_people
+  def create
+    @entry_person = EntryPerson.find_or_create_by(entry_person_params)
+
+    if @entry_person.persisted?
+      redirect_to entry_path(@entry_person.entry_id), notice: "Person was successfully added."
+    else
+      redirect_to entry_path(@entry_person.entry_id), warn: "Person could not be added."
+    end
+  end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def entry_person_params
+      params.require(:entry_person).permit(:entry_id, :person_id)
+    end
 end
