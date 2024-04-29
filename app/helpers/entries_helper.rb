0316a3d7bcb1425d8ba6728b48b85c5084bf4fcc
Esmé Cowles <escowles@ticklefish.org>
2024-04-29 14:51:44 -0400

    moving verbose view code to helpers, adding tags to entry index

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -5,9 +5,28 @@ module EntriesHelper
     end.join(", ").html_safe
   end
 
+  def tag_search(tags)
+    tags.map do |t|
+      link_to t.name, entries_path(q: t.name)
+    end.join(", ").html_safe
+  end
+
   def people_links(people)
     people.map do |p|
       link_to p.name, person_path(p)
+    end.join(", ").html_safe
+  end
+
+  def people_search(people)
+    people.map do |p|
+      link_to p.name, entries_path(q: p.name)
+    end.join(", ").html_safe
+  end
+
+  def location_search(location)
+    location.split(",").map do |loc|
+      loc.strip!
+      link_to loc, entries_path(q: loc)
     end.join(", ").html_safe
   end
 end
