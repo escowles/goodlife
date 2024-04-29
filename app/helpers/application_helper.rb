1cfd002ee3a29b8a97a568d04ee980af16ec61ec
Esmé Cowles <escowles@ticklefish.org>
2024-04-29 19:12:43 -0400

    limiting height of entry description field, fixing delete action and switching to buttons for edit/delete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def inline_button(label, path, method = :get)
+    opts = { form: { style: "display:inline-block;" } }
+    opts[:method] = method if method
+    button_to label, path, opts
+  end
 end
