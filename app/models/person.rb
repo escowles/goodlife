74153c219abafde44b81680bf735942cf4916596
Esmé Cowles <escowles@ticklefish.org>
2024-05-02 14:50:22 -0400

    making tag and person delete associated links with entries

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,4 @@
 class Person < ApplicationRecord
-  has_many :entry_people
+  has_many :entry_people, dependent: :delete_all
   has_many :entries, through: :entry_people
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
-  has_many :entry_tags
+  has_many :entry_tags, dependent: :delete_all
   has_many :entries, through: :entry_tags
 end
